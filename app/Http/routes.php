cfc35f148f08a0eb341a0825066d6c56808e8bdb
najibu <1o1jibby@gmail.com>
2015-10-13 13:29:49 +0000

    Passing data to views

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,20 +1,4 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+Route::get('about', 'PagesController@about');
 
-Route::get('/', function () {
-    return  'Hello World!'; //view('welcome');
-});
-
-Route::get('/contact', function(){
-	return view('pages.contact');
-});
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>About Me:   {{ $first }} {{ $last }} </h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo sit repellat adipisci eum tenetur ad unde aspernatur beatae vero dolores numquam, ut ipsa cupiditate deleniti, ullam optio, animi error consequatur.</p>
+</body>
+</html>

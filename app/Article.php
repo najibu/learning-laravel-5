50a647738f6b1b9c1f06cab648264de139b4a8ec
=najibu <1o1jibby@gmail.com>
2015-10-25 14:34:30 +0000

    Loose Ends and Wrapping Up

--- app/Article.php
+++ app/Article.php
@@ -49,6 +49,11 @@ class Article extends Model
     	$this->attributes['published_at'] = Carbon::parse($date);
     }
 
+    public function getPublishedAtAttribute($date)
+    {
+        return Carbon::parse($date)->format('Y-m-d');    
+    }
+
     /**
      * An article is owned by a user.
      *  
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,15 +2,13 @@
 
 Route::get('foo', 'FooController@foo');
 
-Route::get('/', function ()
-{
-	return 'Home Page';
-});
 
 Route::get('about', 'PagesController@about');
 Route::get('contact', 'PagesController@contact');
 
 Route::resource('articles', 'ArticlesController');
+
+Route::get('tags/{tags}', 'TagsController@show');
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',

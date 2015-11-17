f4f7810ca111670e618620c772dd574d0a2e2016
=najibu <1o1jibby@gmail.com>
2015-11-17 11:52:34 +0000

    Named route groups

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,42 +1,54 @@
 <?php
 
-event(new App\Events\SomeEvent);
+
 
 Route::get('/', function()
 {
 	return view('welcome');
 });
 
-Route::get('admin', [ 'middleware' => 'admin:Gary', function()
+Route::group(['prefix' => 'admin', 'as' => 'Admin::'], function()
 {
-	return 'Hello Gary';
-}]);
-
-Route::get('posts', function()
-{
-	return view('posts')->with('posts', App\Post::all());
+	Route::get('home', ['as' => 'home', function() 
+	{
+		return 'some view';
+	}]);
 });
 
-Route::post('search-results', function(){
-	return sprintf('Search results for "%s"', Request::input('search'));
-});
+dd(route('Admin::home'));
 
-Route::get('other', function()
-{
-	return view('other');
-});
+//dd(route('home'));
 
-Route::get('foo', 'FooController@foo');
+// Route::get('admin', [ 'middleware' => 'admin:Gary', function()
+// {
+// 	return 'Hello Gary';
+// }]);
+
+// Route::get('posts', function()
+// {
+// 	return view('posts')->with('posts', App\Post::all());
+// });
+
+// Route::post('search-results', function(){
+// 	return sprintf('Search results for "%s"', Request::input('search'));
+// });
+
+// Route::get('other', function()
+// {
+// 	return view('other');
+// });
+
+// Route::get('foo', 'FooController@foo');
 
 
-Route::get('about', 'PagesController@about');
-Route::get('contact', 'PagesController@contact');
+// Route::get('about', 'PagesController@about');
+// Route::get('contact', 'PagesController@contact');
 
-Route::resource('articles', 'ArticlesController');
+// Route::resource('articles', 'ArticlesController');
 
-Route::get('tags/{tags}', 'TagsController@show');
+// Route::get('tags/{tags}', 'TagsController@show');
 
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController'
-]);
+// Route::controllers([
+// 	'auth' => 'Auth\AuthController',
+// 	'password' => 'Auth\PasswordController'
+// ]);

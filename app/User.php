d757b02ffa04045278a375a6c05316e8f559e6f0
=najibu <1o1jibby@gmail.com>
2015-11-24 09:31:08 +0000

    Created HasRole class

--- app/User.php
+++ app/User.php
@@ -53,35 +53,6 @@ class User extends Model implements AuthenticatableContract,
         return $this->id == $related->user_id;
     }
 
-    public function roles()
-    {
-        return $this->belongsToMany(Role::class);
-    }
-
-    // $user->hasRole('manager')
-    public function hasRole($role)
-    {
-        if (is_string($role))
-        {
-            return $this->roles->contains('name', $role);
-        }
-
-        return !! $role->intersect($this->roles)->count();
-
-        // foreach ($role as $r) {
-        //     if ($this->hasRole($r->name) {
-        //         return true;
-        //     });
-        // }
-
-        // return false;
-    }
-
-    public function assignRole($role)
-    {
-        return $this->roles()->save(
-            Role::whereName($role)->firstOrFail()
-        );
-    }
+    
 
 }
